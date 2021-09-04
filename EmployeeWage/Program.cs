96b27db5939af7c0e4808c4b475830614b5c6c97
Shubham Mule <shubhambmule@gmail.com>
2021-09-04 16:19:45 +0530

    EmployeWage

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PER_HOUR = 20;
+            int FULL_TIME = 8;
+            int salary;
+            salary = PER_HOUR * FULL_TIME;
+            Console.WriteLine($"Salary is : {salary}");
+
+        }
+    }
+}
