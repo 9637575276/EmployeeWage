86d1533072660a3e2a25af406516a4c02bc83906
Shubham Mule <shubhambmule@gmail.com>
2021-09-04 17:11:19 +0530

    switchcase

---
+++ EmployeeWage/switchcase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class switchcase
+    {
+        static void Main(string[] args)
+        {
+            int PER_HOUR = 20;
+            int FULL_TIME = 8;
+            int PART_TIME = 4;
+            int salary;
+            int rand = new Random().Next(2);
+            salary = PER_HOUR * PART_TIME;
+            switch (rand)
+            {
+                case 0:
+                    Console.WriteLine($"Full Time Salary : {PER_HOUR * FULL_TIME}");
+                    break;
+                case 1:
+                    Console.WriteLine($"Part Time Salary : {PER_HOUR * PART_TIME}");
+                    break;
+            }
+
+        }
+    }
+}
