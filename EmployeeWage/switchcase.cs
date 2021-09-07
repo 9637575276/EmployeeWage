45ad35d460e5bebcc817d2fb366a9d19e27fabe6
Shubham Mule <shubhambmule@gmail.com>
2021-09-07 07:48:56 +0530

    days and hours

--- EmployeeWage/switchcase.cs
+++ EmployeeWage/switchcase.cs
@@ -10,18 +10,38 @@ namespace EmployeeWage
             int FULL_TIME = 8;
             int PART_TIME = 4;
             int salary;
-            int rand = new Random().Next(2);
-            salary = PER_HOUR * PART_TIME;
-            switch (rand)
+            int totalSalary = 0;
+            int WORKING_DAYS = 20;
+            int WORKING_HRS = 100;
+            int workDays = 0;
+            int workHrs = 0;
+            while (workDays < WORKING_DAYS && workHrs <= WORKING_HRS)
             {
-                case 0:
-                    Console.WriteLine($"Full Time Salary : {PER_HOUR * FULL_TIME}");
-                    break;
-                case 1:
-                    Console.WriteLine($"Part Time Salary : {PER_HOUR * PART_TIME}");
-                    break;
+                workDays++;
+                int rand = new Random().Next(3);
+                if (rand == 0)
+                {
+                    salary = FULL_TIME * PER_HOUR;
+                    workHrs = workHrs + FULL_TIME;
+                    totalSalary = totalSalary + salary;
+                }
+                else if (rand == 1)
+                {
+                    salary = PART_TIME * PER_HOUR;
+                    workHrs = workHrs + PART_TIME;
+                    totalSalary = totalSalary + salary;
+                }
+                else
+                {
+                    salary = 0;
+                }
             }
+            Console.WriteLine($"Total Salary : {totalSalary}");
+            Console.WriteLine($"Total Days : {workDays}");
+            Console.WriteLine($"Total Hours : {workHrs}");
 
         }
+
     }
+    
 }
